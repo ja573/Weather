714a263a453464c9a59c389d656692e8e6eabfba
Javier Arias <javier@arias.re>
2021-03-17 16:31:02 +0000

    Implement cache limit

--- src/main/java/com/develogical/CustomForecaster.java
+++ src/main/java/com/develogical/CustomForecaster.java
@@ -9,6 +9,8 @@ import java.util.*;
 
 public class CustomForecaster {
     private Forecaster forecaster;
+    private int cacheLimit = 1;
+    private ArrayList<String> cacheList = new ArrayList<String>();
     private Map<String, CustomForecast> cache = new HashMap<String, CustomForecast>();
 
     public CustomForecaster(Forecaster forecaster) {
@@ -17,16 +19,23 @@ public class CustomForecaster {
 
     public CustomForecast forecastFor(Region region, Day day) {
         String key = region.name() + day.name();
-        if(cache.containsKey(key)){
+        if (cacheList.contains(key)) {
             return cache.get(key);
         }
         Forecast original = this.forecaster.forecastFor(region, day);
         CustomForecast customForecast = new CustomForecast(original.summary(), original.temperature());
+        if (cacheList.size() >= this.cacheLimit) {
+            String oldestKey = cacheList.get(0);
+            cacheList.remove(0);
+            cache.remove(oldestKey);
+        }
+        cacheList.add(key);
         cache.put(key, customForecast );
 
         return customForecast;
     }
 
     public void setLimit(int i) {
+        this.cacheLimit = i;
     }
 }
